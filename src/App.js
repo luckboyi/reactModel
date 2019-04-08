cdd5a2b8b307966d111dae28f73677a16eaf845e
luckboyi <546217953@qq.com>
2019-04-08 20:59:44 +0800

    提交eslint自动格式化代码

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'antd';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+const HomePage = () => (
+  <div>
+    <Button />
+  </div>
+);
+const AboutPage = () => <div>This is a About Page</div>;
+const BaseLayout = () => (
+  <div className="base">
+    <header>
+      <p>react-router demo</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+      </div>
+    </header>
+  </div>
+);
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>demo</h2>
+          <BrowserRouter>
+            <BaseLayout />
+          </BrowserRouter>
         </header>
       </div>
     );
